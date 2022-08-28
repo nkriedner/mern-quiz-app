0599fd387c98d0852880ac164d40723e78ffd65d
Nils Kriedner <nkriedner@googlemail.com>
2022-08-28 09:21:31 +0200

    Create basic express app

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,22 @@
+// Import and use dotenv module for using envirionment variables from .env file:
+require("dotenv").config();
+
+// Import NPM modules:
 const express = require("express");
 
+// Create the express app:
 const app = express();
 
-app.listen(8000, () => console.log("Server started on port 8000."));
+// Middleware:
+app.use((req, res, next) => {
+    console.log(`${req.method} request on ${req.path}`);
+});
+
+// Create route(s):
+app.get("/", (req, res) => {
+    res.json({ mssg: "You are on the / route" });
+});
+
+// Start the express app:
+const port = process.env.PORT;
+app.listen(port, () => console.log(`Server started on port ${port}.`));
