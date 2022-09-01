103f86542a4a2931f5c809e79f826171cc06502f
Nils Kriedner <nkriedner@googlemail.com>
2022-09-01 21:08:34 +0200

    Use bcrypt to hash passwords in the signup route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,7 +8,15 @@ const loginUser = async (req, res) => {
 
 // SIGNUP USER FUNCTION:
 const signupUser = async (req, res) => {
-    res.json({ mssg: "Signup user.." });
+    const { email, password } = req.body; // destructuring email and password from the request body
+
+    try {
+        const user = await User.signup(email, password); // calls the static signup model of the User model
+
+        res.status(200).json({ email, user });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 };
 
 module.exports = { loginUser, signupUser };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 // IMPORTS:
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); // for database connection
+const byrypt = require("bcrypt"); // for hashing passwords
 
 // Initialize a mongoose Schema:
 const Schema = mongoose.Schema;
@@ -16,5 +17,25 @@ const userSchema = new Schema({
         required: true,
     },
 });
+
+// static signup model:
+// (the function needs to be a regular function to use the 'this' keyword)
+userSchema.statics.signup = async function (email, password) {
+    // Check if email already esists in database:
+    const exists = await this.findOne({ email });
+
+    if (exists) {
+        throw Error("An account with this email already exists in the database.");
+    }
+
+    // Generate hashed password:
+    const salt = await byrypt.genSalt(10);
+    const hash = await byrypt.hash(password, salt);
+
+    // Create user:
+    const user = await this.create({ email, password: hash });
+
+    return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
