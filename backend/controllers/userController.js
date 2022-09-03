00a482208ad5d045fbfe460c2e98cf7942dbf671
Nils Kriedner <nkriedner@googlemail.com>
2022-09-03 12:47:55 +0200

    Use jsonwebtoken module to create a user token when signing up

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,10 @@
 // IMPORTS:
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken"); // module for create web token (cookies)
+
+const createToken = (_id) => {
+    return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 // LOGIN USER FUNCTION:
 const loginUser = async (req, res) => {
@@ -13,7 +18,10 @@ const signupUser = async (req, res) => {
     try {
         const user = await User.signup(email, password); // calls the static signup model of the User model
 
-        res.status(200).json({ email, user });
+        // Create Token:
+        const token = createToken(user._id);
+
+        res.status(200).json({ email, token });
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
