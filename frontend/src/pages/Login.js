47b31a47236f9ade92f08486af658b76caae2983
Nils Kriedner <nkriedner@googlemail.com>
2022-09-03 14:18:27 +0200

    Create login and signup form in the frontend

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,29 @@
+import { useState } from "react";
+
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        console.log(email, password);
+    };
+
     return (
         <div>
             <h1>Inside Login Component</h1>
+            <form onSubmit={handleSubmit}>
+                <h2>Log in</h2>
+
+                <label>Email</label>
+                <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+
+                <label>Password</label>
+                <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
+
+                <button>Log in</button>
+            </form>
         </div>
     );
 };
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,7 +1,29 @@
+import { useState } from "react";
+
 const Signup = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        console.log(email, password);
+    };
+
     return (
         <div>
             <h1>Inside Signup Component</h1>
+            <form onSubmit={handleSubmit}>
+                <h2>Sign up</h2>
+
+                <label>Email</label>
+                <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+
+                <label>Password</label>
+                <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
+
+                <button>Sign up</button>
+            </form>
         </div>
     );
 };
