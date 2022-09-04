55c68a4a44275ba60f6f499ef066ca4d98bff997
Nils Kriedner <nkriedner@googlemail.com>
2022-09-04 09:24:30 +0200

    Set the initial Auth status for login and logout logic

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
     const { logout } = useLogout();
+    const { user } = useAuthContext();
 
     const handleClick = () => {
         logout();
@@ -15,21 +17,33 @@ const Navbar = () => {
                     <li>
                         <Link to="/">Introduction</Link>
                     </li>
-                    <li>
-                        <Link to="/play-quiz">Play Quiz</Link>
-                    </li>
-                    <li>
-                        <Link to="/edit-data">Edit Data</Link>
-                    </li>
-                    <li>
-                        <Link to="/signup">Signup</Link>
-                    </li>
-                    <li>
-                        <Link to="/login">Login</Link>
-                    </li>
-                    <li>
-                        <button onClick={handleClick}>Logout</button>
-                    </li>
+                    {user && (
+                        <>
+                            <li>
+                                <Link to="/play-quiz">Play Quiz</Link>
+                            </li>
+                            <li>
+                                <Link to="/edit-data">Edit Data</Link>
+                            </li>
+                            <li>
+                                <button onClick={handleClick}>
+                                    <span>{user.email}</span>
+                                    <br></br>
+                                    Logout
+                                </button>
+                            </li>
+                        </>
+                    )}
+                    {!user && (
+                        <>
+                            <li>
+                                <Link to="/signup">Signup</Link>
+                            </li>
+                            <li>
+                                <Link to="/login">Login</Link>
+                            </li>
+                        </>
+                    )}
                 </ul>
             </nav>
         </header>
